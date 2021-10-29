f9722d1bc14d75c90d5e365ae79a555804e818b4
Ashley W <90473515+s11806981duvalschools@users.noreply.github.com>
2021-10-29 14:43:44 -0400

    version 0.1
    Added string int and floating point data types.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,8 @@
-#Variables Practice, Ashley Wright, 10/29/21 2:23pm, version 0.0
+#Variables Practice, Ashley Wright, 10/29/21 2:39pm, version 0.1
+
+#Three Common Types of Data
+
+lives_remaining = 10 #INTEGERS, any +/- WHOLE number and 0.
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal.
+death_cry = "CURSE YOU, powerpuff girls"
+Line 7 is a STRING -- collection of letters/numbers/other chars.
